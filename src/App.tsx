2600bfce986a68f12b14680ab973a62b212d37ca
Eduardo Queiroz <eduhmq@gmail.com>
2025-06-25 20:39:40 -0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App() {
+  console.log('Hello');
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit
+        neque sed velit faucibus semper. Aliquam enim nisl, pharetra et metus
+        nec, tempor feugiat ipsum. Phasellus elit nisi, cursus et lectus in,
+        ornare commodo justo. Integer sagittis augue id libero lobortis, a
+        consectetur diam commodo. Fusce in porttitor odio. Sed risus metus,
+        pulvinar sit amet quam ac, convallis blandit lacus. Suspendisse
+        imperdiet orci tristique, laci
       </p>
     </>
-  )
+  );
 }
-
-export default App
