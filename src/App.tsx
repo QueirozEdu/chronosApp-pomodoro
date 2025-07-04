87e652552d6d4a6f59b7cfcb0537a526cf67435f
Eduardo Queiroz <eduhmq@gmail.com>
2025-07-03 22:25:55 -0300

    saving current progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,23 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 import { TaskContext, TaskContextProvider } from './contexts/TaskContext';
+import { useState } from 'react';
+
+const initialState: TaskStateModel = {
+  task: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
+  const [state, setState] = useState(initialState);
   return (
     <TaskContextProvider>
       <Home />;
